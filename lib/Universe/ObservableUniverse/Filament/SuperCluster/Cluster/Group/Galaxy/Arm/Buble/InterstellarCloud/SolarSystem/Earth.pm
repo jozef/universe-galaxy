1f8869dc08fca41fdea6127e486ea87573c8fd60
jk <jk@b0c1b02c-7ba7-4df7-b273-855bf36df2ab>
2008-11-28 18:54:47 +0000

    hopefully correct location now
    git-svn-id: https://cle.sk/repos/pub/cpan/Universe-Galaxy/trunk@1113 b0c1b02c-7ba7-4df7-b273-855bf36df2ab

---
+++ lib/Universe/ObservableUniverse/Filament/SuperCluster/Cluster/Group/Galaxy/Arm/Buble/InterstellarCloud/SolarSystem/Earth.pm
@@ -0,0 +1,53 @@
+package Universe::ObservableUniverse::Filament::SuperCluster::Cluster::Group::Galaxy::Arm::Buble::InterstellarCloud::SolarSystem::Earth;
+
+=head1 NAME
+
+Universe::ObservableUniverse::Filament::SuperCluster::Cluster::Group::Galaxy::Arm::Buble::InterstellarCloud::SolarSystem::Earth
+
+=head1 DESCRIPTION
+
+Mostly harmless
+
+=head SYNOPSIS
+
+    use print Universe::ObservableUniverse::Filament::SuperCluster::Cluster::Group::Galaxy::Arm::Buble::InterstellarCloud::SolarSystem::Earth;
+    
+    print Universe::ObservableUniverse::Filament::SuperCluster::Cluster::Group::Galaxy::Arm::Buble::InterstellarCloud::SolarSystem::Earth->info, "\n";
+    print Universe::ObservableUniverse::Filament::SuperCluster::Cluster::Group::Galaxy::Arm::Buble::InterstellarCloud::SolarSystem::Earth->ultimate_answer, "\n";
+
+=cut
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+=head1 METHODS
+
+Any
+
+=cut
+
+sub AUTOLOAD {
+    return 'Mostly harmless';
+}
+
+sub ultimate_answer {
+    return 42;
+}
+
+42;
+
+
+__END__
+
+=head1 CREDITS
+
+Thanks to Pauline Barmby from University of Western Ontario for helping
+me to find out the right location path.
+
+=head1 AUTHOR
+
+Jozef Kutej
+
+=cut
